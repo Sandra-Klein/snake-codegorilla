ac884e8b3fa848ac70e8a51c408683bc3039d071
Jan Struijk <Jan.Struijk95@Gmail.com>
2023-12-04 10:50:29 +0100

    Revert "changed to sources root."
    This reverts commit 8aa5f55019ed68696d033f0ec58e2ec48d207e5d.

--- Snake/Main.java
+++ Snake/Main.java
@@ -1,11 +1,8 @@
 package snake;
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Arrays;
-
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
-
 }
