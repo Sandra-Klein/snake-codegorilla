03e2f178bba01021cb341c7580d01e64d067fd14
Jan Struijk <Jan.Struijk95@Gmail.com>
2023-12-04 13:40:54 +0100

    local changes

--- Snake/snake/Input.java
+++ Snake/snake/Input.java
@@ -14,32 +14,57 @@ enum GameAction {
     MOVE_DOWN,
     MOVE_LEFT,
     MOVE_RIGHT,
-    QUIT
+    GAME_START,
+    GAME_QUIT
 }
 
 public class Input {
     private Scanner scanner;
+    private boolean gameStarted;
+
     public Input() {
         this.scanner = new Scanner(System.in);
+        this.gameStarted = false;
+    }
+    public void startGame() {
+        if (!gameStarted) {
+            System.out.println("Game started, have fun!");
+            gameStarted = true;
+        } else {
+            System.out.println("The game has already started.");
+        }
     }
     public GameAction getUserInput() {
-        System.out.println("Enter your move (W, S, A, D, QUIT): ");
-        String userInput = scanner.nextLine().toUpperCase();
+        if (!gameStarted) {
+            System.out.println("Enter START to begin the game: ");
+            String startInput = scanner.nextLine().toUpperCase();
 
-        switch (userInput) {
-            case "W":
-                return GameAction.MOVE_UP;
-            case "S":
-                return GameAction.MOVE_DOWN;
-            case "A":
-                return GameAction.MOVE_LEFT;
-            case "D":
-                return GameAction.MOVE_RIGHT;
-            case "QUIT":
-                return GameAction.QUIT;
-            default:
-                System.out.println("Invalid input.");
+            if (startInput.equals("START")) {
+                startGame();
+                return GameAction.GAME_START;
+            } else {
+                System.out.println("Invalid input. Enter START to begin the game.");
                 return getUserInput();
+            }
+        } else {
+            System.out.println("Enter your move (W, S, A, D, QUIT): ");
+            String userInput = scanner.nextLine().toUpperCase();
+
+            switch (userInput) {
+                case "W":
+                    return GameAction.MOVE_UP;
+                case "S":
+                    return GameAction.MOVE_DOWN;
+                case "A":
+                    return GameAction.MOVE_LEFT;
+                case "D":
+                    return GameAction.MOVE_RIGHT;
+                case "QUIT":
+                    return GameAction.GAME_QUIT;
+                default:
+                    System.out.println("Invalid input.");
+                    return getUserInput();
+            }
         }
     }
     public void closeScanner() {
@@ -65,7 +90,9 @@ public class Input {
                 case MOVE_RIGHT:
                     System.out.println("Snake moved RIGHT");
                     break;
-                case QUIT:
+                case GAME_START:
+                    break;
+                case GAME_QUIT:
                     System.out.println("Closing game.");
                     gameRunning = false;
                     break;
