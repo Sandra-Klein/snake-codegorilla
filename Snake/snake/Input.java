c54e40bfc298d5c6be05054bb4e2ba0514a16de3
Jan Struijk <Jan.Struijk95@Gmail.com>
2023-12-04 16:35:37 +0100

    local changes

--- Snake/snake/Input.java
+++ Snake/snake/Input.java
@@ -21,10 +21,12 @@ enum GameAction {
 public class Input {
     private Scanner scanner;
     private boolean gameStarted;
+    private Directions lastDirection;
 
     public Input() {
         this.scanner = new Scanner(System.in);
         this.gameStarted = false;
+        this.lastDirection = null;
     }
     public void startGame() {
         if (!gameStarted) {
@@ -52,15 +54,40 @@ public class Input {
 
             switch (userInput) {
                 case "W":
+                    if (lastDirection == Directions.DOWN) {
+                        System.out.println("Can't move UP after moving DOWN.");
+                        return getUserInput();
+                    }
+                    lastDirection = Directions.UP;
                     return GameAction.MOVE_UP;
+
                 case "S":
+                    if (lastDirection == Directions.UP) {
+                        System.out.println("Can't move DOWN after moving UP.");
+                        return getUserInput();
+                    }
+                    lastDirection = Directions.DOWN;
                     return GameAction.MOVE_DOWN;
+
                 case "A":
+                    if (lastDirection == Directions.RIGHT) {
+                        System.out.println("Can't move LEFT after moving RIGHT.");
+                        return getUserInput();
+                    }
+                    lastDirection = Directions.LEFT;
                     return GameAction.MOVE_LEFT;
+
                 case "D":
+                    if (lastDirection == Directions.LEFT) {
+                        System.out.println("Can't move RIGHT after moving LEFT.");
+                        return getUserInput();
+                    }
+                    lastDirection = Directions.RIGHT;
                     return GameAction.MOVE_RIGHT;
+
                 case "QUIT":
                     return GameAction.GAME_QUIT;
+
                 default:
                     System.out.println("Invalid input.");
                     return getUserInput();
