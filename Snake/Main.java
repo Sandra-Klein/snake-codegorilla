8aa5f55019ed68696d033f0ec58e2ec48d207e5d
Jan Struijk <Jan.Struijk95@Gmail.com>
2023-12-04 10:40:25 +0100

    changed to sources root.

--- Snake/Main.java
+++ Snake/Main.java
@@ -1,8 +1,11 @@
 package snake;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
+
 }
