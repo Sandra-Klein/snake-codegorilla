85208225d5aebe8c9da5cf7d01f65510776134e0
Sandra <Sandra.Kleinbergen@hotmail.com>
2023-12-05 13:22:15 +0100

    De stappenplan die we kunnen gebruiken om snake te maken.

--- Snake/snake/Snake.java
+++ Snake/snake/Snake.java
@@ -1,5 +1,21 @@
 package snake;
 
-public class Snake {
+class Coordinate {
+    int x, y;
 
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Coordinate headSnake = new Coordinate(0, 0);
+
+class Snake {
+
+}
+
+
+    public static void main(String[] args) {
+
+    }
 }
