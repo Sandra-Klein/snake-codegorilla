f6960122d3263bfba4f385666a3b1efc952d28e7
Sandra <Sandra.Kleinbergen@hotmail.com>
2023-12-05 11:08:01 +0100

    De stappenplan die we kunnen gebruiken om snake te maken.

---
+++ Snake/snake/Cell.java
@@ -0,0 +1,23 @@
+package snake;
+
+public class Cell {
+    public static void main(String[] args) {
+
+        char[][] gridArray = new char[20][20];
+
+        for (int i = 0; i < gridArray.length; i++) {
+            for (int j = 0; j < gridArray.length; j++) {
+                System.out.println(gridArray[i][j]);
+
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+
