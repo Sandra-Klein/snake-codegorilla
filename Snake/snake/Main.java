7eff685c06a33c5be1799361e1f01079f129a168
Ernstjan Hoek <ernstjanhoek@tutanota.com>
2023-12-05 15:54:22 +0100

    update method doet movement

--- Snake/snake/Main.java
+++ Snake/snake/Main.java
@@ -1,7 +1,5 @@
 package snake;
 import java.util.Scanner;
-import java.util.Random;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
--- Snake/snake/Snake.java
+++ Snake/snake/Snake.java
@@ -2,9 +2,27 @@ package snake;
 
 class Coordinate {
     int x, y;
-
     Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
+    void update(DirEnum option) {
+        switch (option) {
+            case UP:
+                this.y++;
+                break;
+            case DOWN:
+                this.y--;
+                break;
+            case LEFT:
+                this.x--;
+                break;
+            case RIGHT:
+                this.x++;
+                break;
+        }
+    }
+}
+enum DirEnum {
+    UP, DOWN, LEFT, RIGHT
 }
