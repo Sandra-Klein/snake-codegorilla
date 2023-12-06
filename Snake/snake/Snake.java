470eb103ccaf4009f9c723c699a4c32133f2dfa1
Sandra <Sandra.Kleinbergen@hotmail.com>
2023-12-06 13:49:00 +0100

    Coordinates changed.

--- Snake/snake/Snake.java
+++ Snake/snake/Snake.java
@@ -1,7 +1,30 @@
 package snake;
 class Snake {
+    Snake(int value) {
+        coordinateArray = new Coordinate[value];
+        numberOfElements = value;
+    }
 
+    Coordinate[] coordinateArray;
+    int numberOfElements;
+
+    void addCoordinateAtTheBack(Coordinate coordinate) {
+        coordinateArray[numberOfElements] = coordinate;
+        numberOfElements++;
+    }
+
+    void moveSnake(Coordinate coordinate) {
+        for (int i = numberOfElements; i >= 1; i--) {
+            coordinateArray[i] = coordinateArray[i - 1];
+        }
+        coordinateArray[0] = coordinate;
+    }
 }
+
+
+
+
+
 class Coordinate {
     int x, y;
     Coordinate(int x, int y) {
